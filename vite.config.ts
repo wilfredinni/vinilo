e5c81059e8fe2ee8bffdc38940ecf8ac6f137b28
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-06-18 13:02:19 -0400

    auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+        // "vue-i18n",
+        "vue/macros",
+        "@vueuse/head",
+        "@vueuse/core",
+      ],
+      dts: "src/auto-imports.d.ts",
+      dirs: ["src/composables", "src/store"],
+      vueTemplate: true,
+    }),
+  ],
+});
