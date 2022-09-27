9c94a3e801892ea34b7fe357184758a14aabdedf
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-09-27 19:39:51 -0300

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -140,6 +140,6 @@ export default defineConfig({
 
   ssr: {
     // TODO: workaround until they support native ESM
-    noExternal: ['workbox-window', /vue-i18n/],
+    noExternal: [/vue-i18n/],
   },
 })
